2287f46263db3f6c612255a1efd00c9c9e4f3192
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-04-30 12:22:28 -0300

    Consumo de API de login e cadastro de usuario

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -2,20 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   /* ACESSO VAZIO */
   {
-    path: '', redirectTo: 'login', pathMatch: 'full'
+    path: '', 
+    redirectTo: 'login', 
+    pathMatch: 'full'
   },
   /* ============  */
 
   /* MINHA ROTAS DE PAGINACAO */
   {
-    path: 'login', component: EntrarComponent
+    path: 'login', 
+    component: EntrarComponent
   },
   {
-    path: 'cadastrar', component: CadastrarComponent
+    path: 'cadastrar', 
+    component: CadastrarComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 
 ];
--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: User = new User();
+  confirmarSenha: string;
+  tipoUsuario: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  /* QUANDO MINHA PAGINA INICIALIZAR FACA ISSO */
+  ngOnInit() {
+    // FAZ COM QUE O SCROLL SEMPRE FIQUE NO TOPO DA TELA AO INICIALIZAR A APLICACAO
+    window.scroll(0,0)
+  }
+
+  confirmeSenha(event: any) {
+    this.confirmarSenha = event.target.value;
+
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value;
+
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoUsuario;
+
+    if(this.usuario.senha != this.confirmarSenha) {
+      alert('As senhas estao incorretas!');
+
+    }else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: User) => {
+        this.usuario = resp;
+        this.router.navigate(['/login']);
+
+        alert('Usuario cadastrado com sucesso!');
+      });
+      
+    }
+
   }
 
 }
